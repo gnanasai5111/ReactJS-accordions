c0542573f2608a2a510019dbb4edb6846aefa71b
gnanasai <gnanasai5111@gmail.com>
2021-11-12 20:51:27 +0530

    Updates

---
+++ precache-manifest.04ba12c3def64cbf861438a1777522de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aff81cd94d621ea60f1838df6ea89f5",
+    "url": "/ReactJS-accordions/index.html"
+  },
+  {
+    "revision": "bdce741f78158ddb7381",
+    "url": "/ReactJS-accordions/static/css/main.071378ba.chunk.css"
+  },
+  {
+    "revision": "11e2b97d487a12b7b136",
+    "url": "/ReactJS-accordions/static/js/2.f745c462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS-accordions/static/js/2.f745c462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdce741f78158ddb7381",
+    "url": "/ReactJS-accordions/static/js/main.df3c505c.chunk.js"
+  },
+  {
+    "revision": "dc2f69ac34ba08a6615f",
+    "url": "/ReactJS-accordions/static/js/runtime-main.87abff26.js"
+  }
+]);
